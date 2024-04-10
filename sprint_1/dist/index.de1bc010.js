80fe13a888a11d5815b83771c722932d48a4bed5
kryptoded <kao99@bk.ru>
2024-04-10 13:45:35 +0300

    Удалил main из package.json

---
+++ sprint_1/dist/index.de1bc010.js
@@ -0,0 +1,2 @@
+function o(...t){if(!t.length)throw Error("hui sosi pidor");return t.reduce((o,t)=>o+=t,0)}console.log(o(1,2,3,4,5,6,7,8,9,10)),document.querySelector("#root").textContent=o(1,2,3,4,5,6,7,8,9,10).toString();
+//# sourceMappingURL=index.de1bc010.js.map
